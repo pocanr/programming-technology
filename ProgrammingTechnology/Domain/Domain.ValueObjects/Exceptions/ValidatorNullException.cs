26c2ccedc908639615c691dac4c6e9ab1b61ac7c
nikitabulatovvv@gmail.com <nikitabulatovvv@gmail.com>
2026-05-21 19:44:29 +0700

    work in progress

---
+++ ProgrammingTechnology/Domain/Domain.ValueObjects/Exceptions/ValidatorNullException.cs
@@ -0,0 +1,8 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Domain.ValueObjects.Exceptions
+{
+    public class ValidatorNullException(string paramName) : ArgumentNullException(paramName, $"Validator \"{paramName}\" must be specified for type.");
+}
